c51e6f7abc0cff6a65902778f7afd07c978df5a4
SHIVANGI CHOPRA <shivangi@SHIVANGIs-Air.lan>
2024-04-02 16:24:19 -0400

    Creating Topology, Assigning address to nodes

---
+++ simulation.cc
@@ -0,0 +1,73 @@
+#include <fstream>
+#include <string>
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/netanim-module.h"
+#include "ns3/flow-monitor-module.h"
+
+NodeContainer nodes, sender, dest, router, s1tor1, s2tor1, r1tor2, r2tod1, r2tod2;
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+    //creating topology
+    source.Create(2);
+    destination.Create(2);
+    router.Create(2);
+
+    // Create point-to-point helper PointToPointHelper
+    PointToPointHelper pointToPoint;
+
+    //Install Internet stacks on each node
+    InternetStackHelper internet;
+    Ipv4AddressHelper address;
+    internet.Install(source);
+    internet.Install(destination);
+    internet.Install(router);
+
+
+    // Create subnets
+    NodeContainer link1 = NodeContainer(source.Get(0), router.Get(0));
+    NodeContainer link2 = NodeContainer(source.Get(1), router.Get(0));
+    NodeContainer link3 = NodeContainer(router.Get(0), router.Get(1));
+    NodeContainer link4 = NodeContainer(router.Get(1), destination.Get(1));
+    NodeContainer link5 = NodeContainer(router.Get(1), destination.Get(0));
+
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("1Gbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("5ms"));
+
+    //Assigning Ips
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    NetDeviceContainer s1linkr1 = pointToPoint.Install(link1);
+    Ipv4InterfaceContainer i1 = address.Assign(s1linkr1);
+
+
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    NetDeviceContainer s2linkr1 = pointToPoint.Install(link2);
+    Ipv4InterfaceContainer i2 = address.Assign(s2linkr1);
+
+
+    address.SetBase("10.1.3.0", "255.255.255.0");
+    NetDeviceContainer r1linkr2 = pointToPoint.Install(link3);
+    Ipv4InterfaceContainer i3 = address.Assign(r1linkr2);
+
+
+    address.SetBase("10.1.4.0", "255.255.255.0");
+    NetDeviceContainer r2linkd1 = pointToPoint.Install(link5);
+    Ipv4InterfaceContainer i4 = address.Assign(r2linkd1);
+
+    address.SetBase("10.1.5.0", "255.255.255.0");
+    NetDeviceContainer r2linkd2 = pointToPoint.Install(link4);
+    Ipv4InterfaceContainer i5 = address.Assign(r2linkd2);
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+
+}
